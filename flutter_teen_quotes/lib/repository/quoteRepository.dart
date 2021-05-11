4745e9b1afaf0019244798b06a04cce664816e83
alazar-w <ej.dalvic@gmail.com>
2021-05-11 15:09:21 +0300

    repository authorization heder and url added

--- flutter_teen_quotes/lib/repository/quoteRepository.dart
+++ flutter_teen_quotes/lib/repository/quoteRepository.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter_teen_quotes/models/QuoteModel.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_teen_quotes/helper/Constants.dart';
 
 class QuoteRepository{
+
   Future<QuoteModel> getQuoteByTag(String tag) async{
-    final result = await http.Client().get(Uri.parse("http:someurl.com/q=$tag/&APPID = app_id_here"));
+    final result = await http.Client().get(Uri.parse(baseUrl),headers:<String,String>{'authorization':basicAuth});
 
     if(result.statusCode != 200)
       throw Exception();
@@ -12,7 +16,7 @@ class QuoteRepository{
 
   }
   Future<QuoteModel> getQuote() async{
-    final result = await http.Client().get(Uri.parse("http:someurl.com/q=/&APPID = app_id_here"));
+    final result = await http.Client().get(Uri.parse(baseUrl),headers:<String,String>{'authorization':basicAuth});
 
     if(result.statusCode != 200)
       throw Exception();
@@ -23,7 +27,7 @@ class QuoteRepository{
 
   QuoteModel parsedJson(final response){
     final jsonDecoded = json.decode(response);
-    final jsonQuote = jsonDecoded["main"];
+    final jsonQuote = jsonDecoded["datas"]["data"];
 
     return QuoteModel.fromJson(jsonQuote);
   }
